a29739a52ab9c213b6fc18298017558edf222838
MI53RE <er.leguen@free.fr>
2015-10-17 23:49:55 +0200

    levelManager updated

---
+++ UtopJam 2015/Assets/Script/Event/levelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelManager : MonoBehaviour
+{
+
+    public GameObject Menu;
+    public bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") && !isPaused)
+        {
+            StartPause();
+        }
+
+    }
+
+    public void LoadLevel(int id)
+    {
+        Debug.Log("Loading level: " + id);
+        Application.LoadLevel(id);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Bye!");
+        Application.Quit();
+    }
+    public void StartPause()
+    {
+        Debug.Log("Have a Break!");
+        Menu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0;
+    }
+    public void LeavePause()
+    {
+        Debug.Log("Have a Kit-Kat!");
+        Menu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+}
