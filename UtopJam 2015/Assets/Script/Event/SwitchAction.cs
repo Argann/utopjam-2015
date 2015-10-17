95d290a7e70facc534f0f4040b23b7888ec7ea52
MI53RE <er.leguen@free.fr>
2015-10-17 10:08:47 +0200

    prepared workspace

---
+++ UtopJam 2015/Assets/Script/Event/SwitchAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Remoting;
+using UnityEngine;
+using System.Collections;
+
+public class SwitchAction : MonoBehaviour
+{
+    public GameObject[] items;
+
+    public void SwitchItem()
+    {
+        foreach (GameObject item in items)
+        {
+            if (item.GetComponent<Element>().GetIsActive())
+            {
+                item.GetComponent<Element>().Desactivate();
+                Debug.Log(item + "is stopped");
+            }
+            else
+            {
+                item.GetComponent<Element>().Activate();
+            } 
+        }
+    }
+}
