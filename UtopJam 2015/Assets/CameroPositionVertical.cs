5b618084fd62fbe5671b12c12f944556f83e3d82
Etienne <agahnim13@gmail.com>
2015-10-18 04:24:56 +0200

    Level 2 et3 clairement pas finis mais avancés

---
+++ UtopJam 2015/Assets/CameroPositionVertical.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameroPositionVertical : MonoBehaviour {
+    public float distance;
+    public PlayerTrigger target;
+    public Transform theTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Update(){
+        transform.position = new Vector3(-2, theTarget.position.y + 2, theTarget.position.z - distance);
+    }
+}
